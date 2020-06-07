9f90261fcd0bcd864dd10d15e6e93079c59855f7
aditya.hulsure <adityahulsure@gmail.com>
2020-06-07 18:14:16 +0530

    Initial commit : Test Github

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.helloworld
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        print ("onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        print ("onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        print ("onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        print ("onResume")
+    }
+
+    override fun onStop () {
+        super.onStop()
+        print ("onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        print ("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        print ("onDestroy")
+    }
+
+    fun print (msg : String) {
+        Log.d("Activity State : ", msg)
+    }
+}
